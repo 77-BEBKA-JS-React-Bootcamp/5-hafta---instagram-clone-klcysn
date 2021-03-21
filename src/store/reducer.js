b3c775a79df8ace2a524154038d5b2e8a497d73d
klcysn <klcysn@users.noreply.github.com>
2021-03-21 20:47:46 +0100

    Update information

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -11,18 +11,6 @@
           ...state,
           data: [action.payload.newComment, ...state.data]
         };
-      case "GET_COMMENTS_SUCCESS":
-        return {
-          ...state,
-          data: action.payload,
-          isLoading: false
-        };
-      case "GET_COMMENTS_ERROR":
-        return {
-          ...state,
-          error: action.payload,
-          isLoading: false
-        };
       default:
         return state;
     }
